8b3b073f6a4aff7c3b1d70d5864d1a23d51f3900
Leillac88 <leillacarvalho88@gmail.com>
2025-05-27 14:30:48 -0300

    add server #2

--- backend/server.js
+++ backend/server.js
@@ -22,3 +22,39 @@ async function readTodos() {
         throw error;
     }
 }
+
+async function saveTodos(todos) {
+    await fs.writeFile(TODOS_FILE, JSON.stringify(todos, null, 2))
+}
+
+app.use((req, res, next) => {
+    console.log(`${new Data().toISOString()} - ${req.method} ${req.path}`);
+    next();
+})
+
+app.get('/api/todos', async (req, res) => {
+    try {
+        const todos = await readTodos();
+        res.json(todos)
+       
+    } catch (error) {
+        console.error("Erro ao buscar todos: ", error);
+        res.status(500).json({error: "Erro interno do servidor."});
+    }
+})
+
+app.get('/api/todos/:id', async (req, res) => {
+    try {
+        const todos = await readTodos();
+        const todo = todos.find(t => t.id === parseInt(req.params.id));
+
+        if (!todo) {
+            return res.status(404).json({ error: "Tarefa não encontrada." });
+        }
+
+        res.json(todo);
+    } catch (error) {
+        console.error("Erro ao buscar todos: ", error);
+        res.status(500).json({ error: "Erro interno no servidor" });
+    }
+})
